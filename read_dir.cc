508ceb3589fa9f042ae9d4e99917f6f18fbd4905
Sam Newbold <sam@rwsh.org>
2018-11-07 15:59:09 +0100

    Stop using the deprecated readdir_r()

--- read_dir.cc
+++ read_dir.cc
@@ -1,13 +1,13 @@
 // The template used to open a directory, and copy its entries into the
 // container specified by dest.
 //
-// Copyright (C) 2005-2016 Samuel Newbold
+// Copyright (C) 2005-2018 Samuel Newbold
 
 template<class Out> int read_dir(const std::string& filename, Out dest) {
   std::string file = filename + '/';
   DIR *src = opendir(file.c_str());
   if (!src) throw Exception(Argm::Unreadable_dir, file, errno);
-  struct dirent entry, *result = &entry;
-  while (!readdir_r(src, &entry, &result) && result) *dest++ = entry.d_name;
+  struct dirent *result = nullptr;
+  while (result = readdir(src)) *dest++ = result->d_name;
   return closedir(src);}
 
